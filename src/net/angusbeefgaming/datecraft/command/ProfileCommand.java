80e9daedb5bca855d72ea8f881d2ac40269aef20
Atticus Zambrana <atticus.zambrana@angusbeefgaming.net>
2018-08-15 23:15:28 -0700

    Yeet some stuff has been done

--- src/net/angusbeefgaming/datecraft/command/ProfileCommand.java
+++ src/net/angusbeefgaming/datecraft/command/ProfileCommand.java
@@ -11,20 +11,23 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import net.angusbeefgaming.datecraft.player.PlayerManager;
+import net.md_5.bungee.api.ChatColor;
 
 public class ProfileCommand implements CommandExecutor {
 	public boolean onCommand(CommandSender sender, Command cmd, String str, String[] args) {
 		Player player = (Player) sender;
 		
-		Inventory inv = Bukkit.createInventory(null, 18, "My Profile");
+		Inventory inv = Bukkit.createInventory(null, 27, "My Profile");
 		
 		
-		ItemStack gender = nameItem(Material.NAME_TAG, "My Gender: " + PlayerManager.getAccountFromPlayer(player).gender);
-		ItemStack relStatus = nameItem(Material.NAME_TAG, "My Relationship Status: " + PlayerManager.getAccountFromPlayer(player).relationshipStatus);
+		ItemStack gender = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "My Gender: " + PlayerManager.getAccountFromPlayer(player).gender);
+		ItemStack relStatus = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "My Relationship Status: " + PlayerManager.getAccountFromPlayer(player).relationshipStatus);
 		
-		ItemStack relStatusSingle = nameItem(Material.NAME_TAG, "My Relationship Status: Single");
+		ItemStack relStatusSingle = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "My Relationship Status: Single");
+		ItemStack relStatusMarried = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "My Relationship Status: Married");
+		ItemStack relStatusTaken = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "My Relationship Status: Dating");
 		
-		ItemStack together = nameItem(Material.NAME_TAG, "I am with: " + PlayerManager.getAccountFromPlayer(player).togetherWith);
+		ItemStack together = nameItem(Material.NAME_TAG, "" + ChatColor.AQUA + ChatColor.BOLD + "I am with: " + PlayerManager.getAccountFromPlayer(player).togetherWith);
 		
 		
 		if((PlayerManager.getAccountFromPlayer(player).relationshipStatus).equals("NONE")) {
@@ -32,9 +35,16 @@ public class ProfileCommand implements CommandExecutor {
 			inv.setItem(14, relStatusSingle);
 		}
 		else {
-			inv.setItem(11, gender);
-			inv.setItem(13, relStatus);
-			inv.setItem(15, together);
+			if((PlayerManager.getAccountFromPlayer(player).relationshipStatus).equals("MARRIED")) {
+				inv.setItem(11, gender);
+				inv.setItem(13, relStatusMarried);
+				inv.setItem(15, together);
+			}
+			if((PlayerManager.getAccountFromPlayer(player).relationshipStatus).equals("DATING")) {
+				inv.setItem(11, gender);
+				inv.setItem(13, relStatusTaken);
+				inv.setItem(15, together);
+			}
 		}
 		
 		player.openInventory(inv);
